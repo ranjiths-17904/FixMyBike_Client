46670f3f55e56b0d3b7335e76b29f3d80947cc21
Ranjith S <138761903+ranjiths-17904@users.noreply.github.com>
2025-08-17 14:36:16 +0530

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,7 @@
 import api from './api';
 
 export const signupUser = async (userData) => {
-  const res = await api.post('/auth/signup', userData);
+  const res = await api.post('api/auth/signup', userData);
   return res.data;
 };
+
